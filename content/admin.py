0b6a9c7332c4965e11670e994810b6d2733ce317
Henri Kähkönen <henri.kahkonen@gmail.com>
2026-09-16 12:28:04 +0300

    Changed inline forms' extra value to 0

--- content/admin.py
+++ content/admin.py
@@ -39,7 +39,7 @@ class TagForm(forms.ModelForm):
 
 class TagTranslationInline(admin.TabularInline):
     model = TagTranslation
-    extra = 1
+    extra = 0
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -61,7 +61,7 @@ class SnSReleaseForm(forms.ModelForm):
 class SnsPackDescInline(admin.TabularInline):
     '''Language-specific description of a SnS sample pack.'''
     model = SoundsAndScapesPackDescription
-    extra = 1
+    extra = 0
 
 @admin.register(SoundsAndScapesPack)
 class SnSPackAdmin(admin.ModelAdmin):
@@ -75,7 +75,7 @@ class SnSPackAdmin(admin.ModelAdmin):
 
 class SnSChangelogEntryTranslationInline(admin.TabularInline):
     model = SnSChangelogEntryTranslation
-    extra = 1
+    extra = 0
 
 @admin.register(SnSChangelogEntry)
 class SnSChangelogEntryAdmin(admin.ModelAdmin):
@@ -95,7 +95,7 @@ class MusicReleaseForm(forms.ModelForm):
 
 class MusicReleaseTranslationInline(admin.TabularInline):
     model = MusicReleaseTranslation
-    extra = 1
+    extra = 0
 
 @admin.register(MusicRelease)
 class MusicReleaseAdmin(admin.ModelAdmin):
@@ -143,7 +143,7 @@ class ArticleForm(forms.ModelForm):
 
 class ArticleTranslationInline(admin.StackedInline):
     model = ArticleTranslation
-    extra = 1
+    extra = 0
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
@@ -159,7 +159,7 @@ class ArticleAdmin(admin.ModelAdmin):
 
 class ChangelogEntryTranslationInline(admin.TabularInline):
     model = ChangelogEntryTranslation
-    extra = 1
+    extra = 0
 
 @admin.register(ChangelogEntry)
 class ChangelogEntryAdmin(admin.ModelAdmin):
